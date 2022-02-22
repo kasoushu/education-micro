1fc75cb5459478b83f45ebce1b211d74454c8438
kasoushu <kasoushu@gmail.com>
2022-02-23 00:17:53 +0800

    2/22/00:20

--- app/selectCource/internal/biz/course.go
+++ app/selectCource/internal/biz/course.go
@@ -3,11 +3,17 @@ package biz
 import (
 	"context"
 	cv1 "education/api/v1/course"
+	"education/app/selectCource/internal/model"
 	"education/app/selectCource/internal/service"
 	"github.com/go-kratos/kratos/v2/log"
 )
 
 type CourseRepo interface {
+	Create(context.Context, *model.Curriculum) error
+	Update(context.Context, *model.Curriculum, uint64) error
+	Delete(context.Context, uint64) error
+	GetSingle(context.Context, uint64) (*model.CurriculumItem, error)
+	GetListByTeacherId(context.Context, uint64) ([]*model.CurriculumItem, error)
 }
 
 type CourseCase struct {
@@ -23,26 +29,104 @@ func NewCourseCase(crp CourseRepo, logger log.Logger) service.CourseCase {
 }
 
 func (c *CourseCase) Create(ctx context.Context, req *cv1.CreateCourseReq) error {
-	//TODO implement me
-	panic("implement me")
+	err := c.courseRepo.Create(ctx, &model.Curriculum{
+		Name:        req.Name,
+		ClassroomId: req.ClassroomId,
+		GradeYear:   req.GradeYear,
+		Term:        req.Term,
+		Credit:      req.Credit,
+		SchoolHour:  req.SchoolHour,
+		Category:    req.Category,
+		Status:      0,
+		ExamWay:     req.ExamWay,
+		TeacherId:   req.TeacherId,
+	})
+	if err != nil {
+		c.log.Error(err)
+		return err
+	}
+	return nil
 }
 
 func (c *CourseCase) Update(ctx context.Context, req *cv1.UpdateCourseReq) error {
-	//TODO implement me
-	panic("implement me")
+
+	err := c.courseRepo.Update(ctx, &model.Curriculum{
+		Name:        req.Name,
+		ClassroomId: req.ClassroomId,
+		GradeYear:   req.GradeYear,
+		Term:        req.Term,
+		Credit:      req.Credit,
+		SchoolHour:  req.SchoolHour,
+		Category:    req.Category,
+		Status:      req.Status,
+		ExamWay:     req.ExamWay,
+		TeacherId:   req.TeacherId,
+	}, req.Id)
+	if err != nil {
+		c.log.Error(err)
+		return err
+	}
+	return nil
+
 }
 
 func (c *CourseCase) Delete(ctx context.Context, id uint64) error {
-	//TODO implement me
-	panic("implement me")
+	err := c.courseRepo.Delete(ctx, id)
+	if err != nil {
+		c.log.Error(err)
+		return err
+	}
+	return nil
 }
 
 func (c *CourseCase) GetSingle(ctx context.Context, id uint64) (*cv1.CourseInfo, error) {
-	//TODO implement me
-	panic("implement me")
+	info, err := c.courseRepo.GetSingle(ctx, id)
+
+	if err != nil {
+		c.log.Error(err)
+		return nil, err
+	}
+	return &cv1.CourseInfo{
+		Id:            info.Id,
+		ClassroomId:   info.ClassroomId,
+		ClassroomName: info.ClassroomName,
+		TeacherId:     info.TeacherId,
+		TeacherName:   info.TeacherName,
+		Name:          info.Name,
+		GradeYear:     info.GradeYear,
+		Term:          info.Term,
+		Credit:        info.Credit,
+		SchoolHour:    info.SchoolHour,
+		Category:      info.Category,
+		Status:        info.Status,
+		ExamWay:       info.ExamWay,
+	}, nil
+
 }
 
 func (c *CourseCase) GetListCourseByTeacherID(ctx context.Context, id uint64) (*cv1.CourseListReply, error) {
-	//TODO implement me
-	panic("implement me")
+	list, err := c.courseRepo.GetListByTeacherId(ctx, id)
+	if err != nil {
+		c.log.Error(err)
+		return nil, err
+	}
+	res := make([]*cv1.CourseInfo, 0)
+	for _, info := range list {
+		res = append(res, &cv1.CourseInfo{
+			Id:            info.Id,
+			ClassroomId:   info.ClassroomId,
+			ClassroomName: info.ClassroomName,
+			TeacherId:     info.TeacherId,
+			TeacherName:   info.TeacherName,
+			Name:          info.Name,
+			GradeYear:     info.GradeYear,
+			Term:          info.Term,
+			Credit:        info.Credit,
+			SchoolHour:    info.SchoolHour,
+			Category:      info.Category,
+			Status:        info.Status,
+			ExamWay:       info.ExamWay,
+		})
+	}
+	return &cv1.CourseListReply{List: res}, nil
 }
--- app/selectCource/internal/model/curriculum.go
+++ app/selectCource/internal/model/curriculum.go
@@ -2,7 +2,6 @@ package model
 
 import (
 	"gorm.io/gorm"
-	"time"
 )
 
 type Curriculum struct {
@@ -10,15 +9,28 @@ type Curriculum struct {
 	Name        string
 	Id          uint64 `gorm:"<-:false;primaryKey;autoIncrement"`
 	ClassroomId uint64
-	StartTime   time.Time
-	EndTime     time.Time
-	Credit      int
-	SchoolHour  int
+	GradeYear   int32
+	Term        int32
+	Credit      int32
+	SchoolHour  int32
 	Category    string
-	Status      int    // 0,1,2,3,4
+	Status      int32  // 0,1,2,3,4
 	ExamWay     string //
 	TeacherId   uint64
 }
 
 type CurriculumItem struct {
+	Id            uint64
+	ClassroomId   uint64
+	ClassroomName string
+	TeacherId     uint64
+	TeacherName   uint64
+	Name          string
+	GradeYear     int32
+	Term          int32
+	Credit        int32
+	SchoolHour    int32
+	Category      string
+	Status        int32
+	ExamWay       string
 }
